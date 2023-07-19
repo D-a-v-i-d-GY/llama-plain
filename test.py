7c24ecd7d36ddca897471f8c60da888f602a456c
David <dg615@cam.ac.uk>
2023-07-19 14:27:27 +0100

    simple test of the model

--- test.py
+++ test.py
@@ -4,9 +4,17 @@ from typing import Optional
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 # Load model directly
+from transformers.models.llama import LlamaTokenizer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-pipe = pipeline("text-generation", model="Cheng98/llama-160m")
-
-tokenizer = AutoTokenizer.from_pretrained("Cheng98/llama-160m")
+gpu = torch.device("cuda:0")
+tokenizer = LlamaTokenizer.from_pretrained("Cheng98/llama-160m")
 model = AutoModelForCausalLM.from_pretrained("Cheng98/llama-160m")
+pipe = pipeline(
+    "text-generation", 
+    model=model, 
+    tokenizer=tokenizer, 
+    framework="pt", 
+    device=gpu,
+    )
+print(pipe("", return_full_text=True, max_new_tokens=50)[0]["generated_text"])
