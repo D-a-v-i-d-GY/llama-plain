53e08c965688a5406837ee069dde4649df782614
David <dg615@cam.ac.uk>
2023-07-28 15:12:02 +0100

    mqa text generation comparison

--- test.py
+++ test.py
@@ -1,20 +1,42 @@
-import torch
-from typing import Optional
-
-# Use a pipeline as a high-level helper
+from transformers.models.llama import LlamaTokenizer, LlamaForCausalLM
 from transformers import pipeline
-# Load model directly
-from transformers.models.llama import LlamaTokenizer
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import numpy as np
+import os
+import modeling_llama_mqa
+import modeling_llama
+from mqa import mha2mqa
+from data_module import MyDataModule
+from datasets import load_dataset
+from torch.utils.data import DataLoader
 
-gpu = torch.device("cuda:0")
-tokenizer = LlamaTokenizer.from_pretrained("Cheng98/llama-160m")
-model = AutoModelForCausalLM.from_pretrained("Cheng98/llama-160m")
-pipe = pipeline(
-    "text-generation", 
-    model=model, 
-    tokenizer=tokenizer, 
-    framework="pt", 
-    device=gpu,
-    )
-print(pipe("", return_full_text=True, max_new_tokens=50)[0]["generated_text"])
+
+model_name = "Cheng98/llama-160m"
+
+
+model = LlamaForCausalLM.from_pretrained(model_name)
+tokenizer = LlamaTokenizer.from_pretrained(model_name)
+my_mqa_model = modeling_llama_mqa.LlamaForCausalLM(model.config)
+my_mqa_model_random = modeling_llama_mqa.LlamaForCausalLM(model.config)
+my_mqa_model_transpose = modeling_llama_mqa.LlamaForCausalLM(model.config)
+
+state = model.state_dict()
+state = mha2mqa(state, num_layers=12, num_heads=12)
+my_mqa_model_transpose.load_state_dict(state)
+
+state = model.state_dict()
+state = mha2mqa(state, num_layers=12, num_heads=12, transpose_layer=False)
+my_mqa_model.load_state_dict(state)
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=40)
+generate_ids_mqa = my_mqa_model.generate(inputs.input_ids, max_length=40)
+generate_ids_mqa_random = my_mqa_model.generate(inputs.input_ids, max_length=40)
+generate_ids_mqa_transpose = my_mqa_model.generate(inputs.input_ids, max_length=40)
+print(tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0], end="\n")
+print(tokenizer.batch_decode(generate_ids_mqa, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0], end="\n")
+print(tokenizer.batch_decode(generate_ids_mqa_random, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0], end="\n")
+print(tokenizer.batch_decode(generate_ids_mqa_transpose, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0], end="\n")
