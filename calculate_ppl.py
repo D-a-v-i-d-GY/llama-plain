49a3439dc6e764224c85800d0a204c9349770af1
David <dg615@cam.ac.uk>
2023-08-10 14:21:32 +0100

    test after arch. error fix

--- calculate_ppl.py
+++ calculate_ppl.py
@@ -40,53 +40,50 @@ def calculate_ppl(model, encodings, device, stride=512, max_length=1024):
 
 device = 'cuda'
 model_name = "Cheng98/llama-160m"
-torch.manual_seed(42)
-
-groups_idx = [[[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11]]] * 12
+torch.manual_seed(420)
 
 tokenizer = LlamaTokenizer.from_pretrained(model_name)
-
-model = LlamaForCausalLM.from_pretrained(model_name).to(device)
-#my_model = LlamaForCausalLM(model.config).to(device)
-my_model_random = LlamaForCausalLM(model.config).to(device)
-#my_mqa_model = modeling_llama_mqa.LlamaForCausalLM(model.config).to(device)
-#my_mqa_model_random = modeling_llama_mqa.LlamaForCausalLM(model.config).to(device)
-model.config.groups_idx = groups_idx
-gqa_model = modeling_llama_gqa.LlamaForCausalLM(model.config).to(device)
-#gqa_model_random = modeling_llama_gqa.LlamaForCausalLM(model.config).to(device)
-state = model.state_dict()
-gqa_model.load_state_dict(mha2gqa(state, groups_idx, num_heads=12, transpose_layer=False))
-
-#state = model.state_dict()
-#my_model.load_state_dict(state)
-
-#state = mha2mqa(state, num_layers=12, num_heads=12, transpose_layer=True)
-#my_mqa_model.load_state_dict(state)
 
 test = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
 encodings = tokenizer("\n\n".join(test["text"]), return_tensors="pt").to(device)
 
+groups_idx = [[[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11]]] * 12
+
+model = LlamaForCausalLM.from_pretrained(model_name).to(device)
+model_random = LlamaForCausalLM(model.config).to(device)
+mqa_model = modeling_llama_mqa.LlamaForCausalLM(model.config).to(device)
+mqa_model_random = modeling_llama_mqa.LlamaForCausalLM(model.config).to(device)
+
+model.config.groups_idx = groups_idx
+gqa_model = modeling_llama_gqa.LlamaForCausalLM(model.config).to(device)
+gqa_model_random = modeling_llama_gqa.LlamaForCausalLM(model.config).to(device)
+
+state = model.state_dict()
+gqa_model.load_state_dict(mha2gqa(state, groups_idx, num_heads=12, transpose_layer=False))
+
+state = model.state_dict()
+mqa_model.load_state_dict(mha2mqa(state, num_layers=12, num_heads=12, transpose_layer=True))
+
+
 with torch.inference_mode():
     model.eval()
-#    my_model.eval()
-    my_model_random.eval()
-#    my_mqa_model.eval()
-#    my_mqa_model_random.eval()
-#    my_gqa_model.eval()
-#    my_gqa_model_random.eval()
+    model_random.eval()
+    mqa_model.eval()
+    mqa_model_random.eval()
+    gqa_model.eval()
+    gqa_model_random.eval()
+
     ppl = calculate_ppl(model, encodings, device)
-#    ppl_ = calculate_ppl(my_model, encodings, device)
-    ppl_random = calculate_ppl(my_model_random, encodings, device)
-#    ppl_mqa = calculate_ppl(my_mqa_model, encodings, device)
-#    ppl_mqa_random = calculate_ppl(my_mqa_model_random, encodings, device)
+    ppl_random = calculate_ppl(model_random, encodings, device)
+    ppl_mqa = calculate_ppl(mqa_model, encodings, device)
+    ppl_mqa_random = calculate_ppl(mqa_model_random, encodings, device)
     ppl_gqa = calculate_ppl(gqa_model, encodings, device)
-#    ppl_gqa_random = calculate_ppl(gqa_model_random, encodings, device)
+    ppl_gqa_random = calculate_ppl(gqa_model_random, encodings, device)
 
 
 print("base: ", ppl)
-#print("base model, loaded weights: ", ppl_)
 print("base model, random weights: ", ppl_random)
-#print("MHA -> MQA, transformed weights: ", ppl_mqa)
-#print("MHA -> MQA, random weights: ", ppl_mqa_random)
-print(f"MHA -> GQA, transformed weights: ", ppl_gqa)
-#print(f"MHA -> GQA, random weights: ", ppl_gqa_random, end="\n")
+print("MHA -> MQA, transformed weights: ", ppl_mqa)
+print("MHA -> MQA, random weights: ", ppl_mqa_random)
+print(f"MHA -> GQA with {len(groups_idx[0])} groups, transformed weights: ", ppl_gqa)
+print(f"MHA -> GQA with {len(groups_idx[0])} groups, random weights: ", ppl_gqa_random, end="\n")
