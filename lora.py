ee307c4991c50f54d2c546f915b07c9646dfc96d
David <dg615@cam.ac.uk>
2023-08-22 11:37:58 +0100

    change model to save, add comments

--- lora.py
+++ lora.py
@@ -9,9 +9,10 @@ from peft import get_peft_model, LoraConfig
 
 def main():
 
+    # Model Parameters
     model_name = "Cheng98/llama-160m"
     batch_size = 4
-    max_epochs: int = 2
+    max_epochs: int = 1
     max_steps: int = -1
     r = 8
     lora_alpha = 8
@@ -19,19 +20,21 @@ def main():
     learning_rate: float = 2e-4
     num_warmup_steps: int = 0
     
+    # Load the model and tokenizer
     model = LlamaForCausalLM.from_pretrained(
         model_name, 
-        torch_dtype=torch.float16,
         device_map="auto",
     )
 
     tokenizer = LlamaTokenizer.from_pretrained(model_name)
 
+    # Freeze non-LoRA parameters 
     for param in model.parameters():
         param.requires_grad = False
         if param.ndim == 1:
             param.data = param.data.to(torch.float32)
 
+    # Peft config for LoRA
     peft_config = LoraConfig(
         r=r, 
         lora_alpha=lora_alpha, 
@@ -44,8 +47,9 @@ def main():
     model = get_peft_model(model, peft_config)
     model.print_trainable_parameters()
 
+    # Dataset
     train_data = load_dataset("wikitext", "wikitext-2-raw-v1", split="train")
-    train_data_enc = train_data.map(lambda x: tokenizer(x["text"]))
+    train_data_enc = train_data.map(lambda x: tokenizer(x["text"]), batched=True)
 
     trainer = transformers.Trainer(
         model=model,
@@ -71,7 +75,11 @@ def main():
 
     index = len(os.listdir("lora_models/"))
     model_id = f"lora_models/plain-lora-{index}"
-    model.save_pretrained(model_id)
+
+    model_to_save = trainer.model.module if hasattr(trainer.model, "module") else trainer.model
+    model_to_save.save_pretrained(model_id)
+
+    #model.save_pretrained(model_id)
 
 if __name__ == "__main__":
     main()
