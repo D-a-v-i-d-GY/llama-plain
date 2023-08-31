7b15b4101a649e63bb0cb7ea62a6d85ea8114dd6
David <dg615@cam.ac.uk>
2023-08-31 13:35:33 +0100

    change file paths for gqa

--- accelerate_fsdp_llama_lora_gqa.py
+++ accelerate_fsdp_llama_lora_gqa.py
@@ -26,7 +26,7 @@ def main():
     # model_name = "lmsys/vicuna-7b-v1.3"
     # lora_config_path = parse_arguments() --> The following is used to pass a .toml file throught the CLI e.g --lora-config-path machop/configs/by_model/llama_lora/lora_by_type.toml
     config_files = [
-        "lora_by_type.toml",
+        "lora_by_type_gqa.toml",
     ]
     task = "lm"
     dataset_name = "wikitext2"
@@ -41,7 +41,7 @@ def main():
     weight_decay: float = 0.0
     lr_scheduler_type: str = "linear"
     num_warmup_steps: int = 0
-    save_path: str = "./ckpts-llama-lora-plain"
+    save_path: str = "./ckpts-llama-lora-gqa"
     load_name: str = None
     load_type: str = ""
 
