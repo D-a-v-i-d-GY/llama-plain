f09df004abc03eeceda86f007750ce1da099e095
David <dg615@cam.ac.uk>
2023-09-01 17:54:35 +0100

    update grouping description

--- gqa-optuna.py
+++ gqa-optuna.py
@@ -59,7 +59,8 @@ def evaluate(model, task, eval_dataloader, device, step_stop=-1):
 
 def n_uniform_groups(num_groups, num_heads, num_layers, depth=-1, reverse=False):
     group_size = num_heads // num_groups
-    print(f"Uniform grouping with {num_groups} groups, group size = {group_size}, and grouping layer depth = {depth}")
+    if reverse: print(f"Reversed uniform grouping with {num_groups} groups, group size = {group_size}, and grouping layer depth = {depth}")
+    else: print(f"Uniform grouping with {num_groups} groups, group size = {group_size}, and grouping layer depth = {depth}")
     mha = [[[0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11]]]
     reverse = 1 if reverse else 0
 
